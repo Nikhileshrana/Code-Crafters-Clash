d9c46c578f59ab55e7ca4e2879ada7746a7690c7
Nikhilesh Rana <101921995+Nikhileshrana@users.noreply.github.com>
2023-10-20 00:38:16 +0530

    Delete files/Contact directory

--- files/Contact/script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn');
-
-function classAdd(){
- const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-}
- btn.addEventListener('click', classAdd);
